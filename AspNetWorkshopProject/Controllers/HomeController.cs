afed38159c561160ec973018dfd9777fe1cc0495
Rodrigo Muniz <rodrigo.muniz.dev@gmail.com>
2020-07-07 18:24:04 -0300

    Razor pages tests

--- AspNetWorkshopProject/Controllers/HomeController.cs
+++ AspNetWorkshopProject/Controllers/HomeController.cs
@@ -18,6 +18,7 @@ namespace AspNetWorkshopProject.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
+            ViewData["Email"] = "rodrigo.muniz.dev@gmail.com";
 
             return View();
         }
